cd4736ce7f014c98aa38ef16146c91fd994bc3cc
Caden Jiang <jiangshen16@icloud.com>
2017-02-25 14:01:48 -0500

    made recycler view

---
+++ app/src/main/java/smarta/smarta/RVAdapter.java
@@ -0,0 +1,33 @@
+package smarta.smarta;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by jiangshen on 2/25/17.
+ */
+
+//public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CustomViewHolder> {
+//
+//    @Override
+//    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+//        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.trip_report_card, null);
+//        CustomViewHolder viewHolder = new CustomViewHolder(view);
+//        return viewHolder;
+//    }
+//
+//    class CustomViewHolder extends RecyclerView.ViewHolder {
+//        protected TextView trainID;
+//        protected TextView srcToDst;
+//        protected TextView
+//
+//        public CustomViewHolder(View view) {
+//            super(view);
+//
+//        }
+//    }
+//}

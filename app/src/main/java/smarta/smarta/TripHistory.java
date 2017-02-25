d028542f367ab98f8774e264fab624269772e6f2
Caden Jiang <jiangshen16@icloud.com>
2017-02-25 02:53:08 -0500

    Added TripHistory

---
+++ app/src/main/java/smarta/smarta/TripHistory.java
@@ -0,0 +1,13 @@
+package smarta.smarta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_history);
+    }
+}
